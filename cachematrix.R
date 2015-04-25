2631638bf503bcb4e13252b324cc69ba14f38640
Jun Chen <junchen1986@gmail.com>
2015-04-25 14:38:12 -0600

    finished assignment on 4/15/2015 in Starbucks

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    mInv <- NULL
+    set <- function(y) {
+        x <<- y
+        mInv <<- NULL
+    }
+    get <- function() x
+    setInv <- function(matInv) mInv <<- matInv
+    getInv <- function() mInv
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
 
 }
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    mInv <- x$getInv()
+    if(!is.null(mInv)) {
+        message("getting cached data")
+        return(mInv)
+    }
+    data <- x$get()
+    mInv <- solve(data, ...)
+    x$setInv(mInv)
+    mInv    
 }
